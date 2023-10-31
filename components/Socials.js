fe2d36fe06f745a7f364d008b3e2fd55719221ec
jandierekhb.arroyo <jandierekhb.arroyo@gmail.com>
2023-10-31 18:28:28 +0800

    added header social feature icons

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,37 @@
+// link
+import Link from "next/link"
+
+// icons
+import {RiYoutubeLine, RiInstagramLine, RiFacebookBoxLine, RiDribbbleLine, RiBehanceLine, RiPinterestLine, RiLinkedinLine, RiGithubLine} from 'react-icons/ri'
+
+
 const Socials = () => {
-  return <div>Socials</div>;
+
+  return (
+    <section className='flex items-center gap-x-5 text-lg'>
+
+  
+      <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <RiInstagramLine/>
+      </Link>
+
+      <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <RiFacebookBoxLine/>
+      </Link>
+
+      <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <RiLinkedinLine/>
+      </Link>
+
+
+      <Link href={''} className='hover:text-accent transition-all duration-300'>
+        <RiGithubLine/>
+      </Link>
+
+
+
+    </section>
+  )
 };
 
 export default Socials;
